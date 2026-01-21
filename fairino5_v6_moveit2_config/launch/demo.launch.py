d0177f350339e965fdf2c479f67940895cf9c08b
Gopi-rainierlabs <gopi@rainierlabs.ai>
2026-01-21 12:10:42 -0800

    modified according to requirements

--- fairino5_v6_moveit2_config/launch/demo.launch.py
+++ fairino5_v6_moveit2_config/launch/demo.launch.py
@@ -1,7 +1,19 @@
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_demo_launch
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch import LaunchDescription
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("fairino5_v6_robot", package_name="fairino5_v6_moveit2_config").to_moveit_configs()
-    return generate_demo_launch(moveit_config)
+    use_hardware = LaunchConfiguration('use_hardware')
+    declare_hw = DeclareLaunchArgument('use_hardware', default_value='true')
+
+    moveit_config = (
+        MoveItConfigsBuilder("fairino5_v6_robot", package_name="fairino5_v6_moveit2_config")
+        .robot_description(mappings={'use_hardware': use_hardware})
+        .to_moveit_configs()
+    )
+    demo_ld = generate_demo_launch(moveit_config)
+    # Prepend the argument declaration to the generated launch
+    return LaunchDescription([declare_hw, *demo_ld.entities])
